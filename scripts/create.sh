740b40f08ccafba314e4dabb603670bf7b34f533
Rafael David Tinoco <rafael.tinoco@linaro.org>
2018-07-22 17:17:18 -0300

    more submodules

---
+++ scripts/create.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+OLDDIR=$PWD
+MAINDIR=$(dirname $0)
+
+getout() {
+    echo ERROR: $@
+    exit 1
+}
+
+gitclone() {
+    name=$1
+    url=$2
+
+    echo ====
+    echo CLONING: $1
+    git submodule add -f $url $name
+}
+
+cd $MAINDIR
+
+FILE=$(ls -1 1_* | head -1)
+
+[ ! -f $FILE ] && getout no trees file found
+
+while read name url
+do
+    [ -d $name ] && continue
+    gitclone $name $url
+    
+done < $FILE
+
+cd $OLDDIR
