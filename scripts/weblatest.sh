de15932f16e981b014197c2294fa2513a28e5447
Rafael David Tinoco <rafael.tinoco@linaro.org>
2018-08-15 18:46:42 -0300

    fixed weblatest

--- scripts/weblatest.sh
+++ scripts/weblatest.sh
@@ -14,18 +14,21 @@ OLDDIR=$PWD
 cd $MAINDIR
 
 [ ! -d latest ] && mkdir latest
-rm -f latest/*
+rm -rf latest/*
 
 for arch in $(ls -1 | grep -v latest); do
+
     for pkg in $(ls $arch); do
+
+        mkdir -p latest/$pkg/$arch/
 
         deb=$(ls -t1 $arch/$pkg/*.deb 2> /dev/null | tail -1)
         rpm=$(ls -t1 $arch/$pkg/*.rpm 2> /dev/null | tail -1)
         tgz=$(ls -t1 $arch/$pkg/*.tgz 2> /dev/null | tail -1)
 
-        [ $deb ] && ln -s ../$deb ./latest/$(basename $deb)
-        [ $rpm ] && ln -s ../$rpm ./latest/$(basename $rpm)
-        [ $tgz ] && ln -s ../$tgz ./latest/$(basename $tgz)
+        [ $deb ] && ln -s ../../../$deb ./latest/$pkg/$arch/$(basename $deb)
+        [ $rpm ] && ln -s ../../../$rpm ./latest/$pkg/$arch/$(basename $rpm)
+        [ $tgz ] && ln -s ../../../$tgz ./latest/$pkg/$arch/$(basename $tgz)
 
     done
 done
