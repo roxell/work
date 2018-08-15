192a09bd042645d51184a84dd9f1c82c3aa73d80
Rafael David Tinoco <rafael.tinoco@linaro.org>
2018-08-15 18:13:27 -0300

    fixed weblatested for last file

--- scripts/weblatest.sh
+++ scripts/weblatest.sh
@@ -19,9 +19,9 @@ rm -f latest/*
 for arch in $(ls -1 | grep -v latest); do
     for pkg in $(ls $arch); do
 
-        deb=$(ls -t1 $arch/$pkg/*.deb 2> /dev/null)
-        rpm=$(ls -t1 $arch/$pkg/*.rpm 2> /dev/null)
-        tgz=$(ls -t1 $arch/$pkg/*.tgz 2> /dev/null)
+        deb=$(ls -t1 $arch/$pkg/*.deb 2> /dev/null | tail -1)
+        rpm=$(ls -t1 $arch/$pkg/*.rpm 2> /dev/null | tail -1)
+        tgz=$(ls -t1 $arch/$pkg/*.tgz 2> /dev/null | tail -1)
 
         [ $deb ] && ln -s ../$deb ./latest/$(basename $deb)
         [ $rpm ] && ln -s ../$rpm ./latest/$(basename $rpm)
